76bb9ef8f86040305fc4863df5e1a3ba99e7dad2
Lauraalexandra9 <lauraalexandra.aj@hotmail.com>
2022-08-14 11:11:36 +0300

    [change] - finishing implementation of trip class and all rquired ones

---
+++ src/main/java/com/sda/travelagency/controller/TripController.java
@@ -0,0 +1,9 @@
+package com.sda.travelagency.controller;
+
+import com.sda.travelagency.entity.Trip;
+import com.sda.travelagency.service.TripService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+public class TripController {
+}
---
+++ src/main/java/com/sda/travelagency/service/TripService.java
@@ -0,0 +1,18 @@
+package com.sda.travelagency.service;
+
+import com.sda.travelagency.entity.Trip;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+import java.util.List;
+
+@Service
+@Slf4j
+public class TripService {
+    public List<Trip> findAllTrips() {
+        log.info("finding all trips");
+
+        return Collections.emptyList();
+    }
+}
