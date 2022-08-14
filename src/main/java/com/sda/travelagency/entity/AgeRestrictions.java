ea3542e00f381464971fb607f5da2a533b3de5d6
Lauraalexandra9 <lauraalexandra.aj@hotmail.com>
2022-08-14 10:25:59 +0300

    [change] - finishing implementation of trip class and all rquired ones

--- src/main/java/com/sda/travelagency/entity/AgeRestrictions.java
+++ src/main/java/com/sda/travelagency/entity/AgeRestrictions.java
@@ -1,13 +1,8 @@
 package com.sda.travelagency.entity;
 
-import lombok.Data;
-import lombok.AllArgsConstructor;
-
-@Data
-@AllArgsConstructor
 public enum AgeRestrictions {
     NO_LIMIT,
     KIDS_ONLY,
-    ADULT_ONLY,
+    ADULT_ONLY
 
 }
