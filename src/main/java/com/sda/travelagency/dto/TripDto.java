9f9253f0c1fe5d9556c1cd39b125f1951e7b9dde
Lauraalexandra9 <lauraalexandra.aj@hotmail.com>
2022-08-21 09:39:59 +0300

    [change] - enumeration package

--- src/main/java/com/sda/travelagency/dto/TripDto.java
+++ src/main/java/com/sda/travelagency/dto/TripDto.java
@@ -1,19 +1,28 @@
-package com.sda.travelagency.converter;
+package com.sda.travelagency.dto;
 
-import com.sda.travelagency.dto.TripDto;
-import com.sda.travelagency.entity.Trip;
-import org.springframework.stereotype.Component;
+import com.sda.travelagency.entity.Destination;
+import com.sda.travelagency.entity.HotelFacilities;
+import com.sda.travelagency.entity.Price;
+import com.sda.travelagency.entity.SecurityRules;
+import com.sda.travelagency.entity.enumeration.MealType;
+import com.sda.travelagency.entity.enumeration.PaymentType;
+import com.sda.travelagency.entity.enumeration.TransportType;
+import lombok.Builder;
 
-@Component
-public class TripConverter implements Converter<Trip, TripDto> {
-    @Override
-    public TripDto fromEntityToDto(Trip trip) {
+import java.time.LocalDate;
+import java.util.List;
 
-        return null;
-    }
-
-    @Override
-    public Trip fromDtoToEntity(TripDto tripDto) {
-        return null;
-    }
+@Builder
+public record TripDto(
+        LocalDate tripStartDate,
+        LocalDate tripEndDate,
+        Destination destination, // TODO: use dto
+        Price tripPrice, // TODO: should we use dto???
+        TransportType typeOfTransport,
+        SecurityRules securityRules, // TODO: use dto
+        PaymentType paymentType,
+        MealType mealType,
+        HotelFacilities hotelFacilities, // TODO: use dto
+        List<String> photos
+) {
 }
