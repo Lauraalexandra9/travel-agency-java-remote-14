c3dcd06fed8ff430754eda6e8d9900987e392228
Lauraalexandra9 <lauraalexandra.aj@hotmail.com>
2022-09-03 10:43:50 +0300

    [change] - hotel facilities converter

--- backend/src/main/java/com/sda/travelagency/dto/HotelFacilitiesDto.java
+++ backend/src/main/java/com/sda/travelagency/dto/HotelFacilitiesDto.java
@@ -1,12 +1,10 @@
 package com.sda.travelagency.dto;
 
-import com.sda.travelagency.entity.Attraction;
-
 import java.util.List;
 
 
 public record HotelFacilitiesDto (
-        List<Attraction> attractions,
+        List<AttractionDto> attractions,
 
         List<String> apartmentFacilities
 
--- backend/src/main/java/com/sda/travelagency/entity/HotelFacilities.java
+++ backend/src/main/java/com/sda/travelagency/entity/HotelFacilities.java
@@ -6,6 +6,7 @@ import lombok.Data;
 import java.util.List;
 @Data
 @AllArgsConstructor
+
 public class HotelFacilities {
     List<Attraction> attractions;
 
