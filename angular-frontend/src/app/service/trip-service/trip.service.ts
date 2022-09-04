93e74ddba678da3c306583d0743f03341cedf419
Lauraalexandra9 <lauraalexandra.aj@hotmail.com>
2022-09-04 10:23:04 +0300

    [add] - getAllTrip function implemented - first approach

--- angular-frontend/src/app/service/trip-service/trip.service.ts
+++ angular-frontend/src/app/service/trip-service/trip.service.ts
@@ -1,10 +1,15 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 
+
 @Injectable({
   providedIn: 'root'
 })
 export class TripService {
 // TODO: add correct module to AppModule
   constructor(private http: HttpClient) { }
+
+  getAllTrip(): any {
+    this.http.get<any[]>(allTripsUrl)
+  }
 }
